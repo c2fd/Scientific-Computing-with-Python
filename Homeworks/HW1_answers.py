0339e034a91dc0973a480722aa74a4e7b6de6818
JiaZhaoMath <89438129+JiaZhaoMath@users.noreply.github.com>
2021-09-15 22:01:35 -0600

    Update HW1_answers.py

--- Homeworks/HW1_answers.py
+++ Homeworks/HW1_answers.py
@@ -1,5 +1,16 @@
 # Version I
+# A number is said to be Disarium if the sum of its digits raised to their respective positions is the number itself.
+#Create a function that determines whether a number is a Disarium or not. Note: Please upload your final answer in a .py file.
 
+#Examples
+#is_disarium(75) ➞ False
+# 7^1 + 5^2 = 7 + 25 = 32
+#is_disarium(135) ➞ True
+# 1^1 + 3^2 + 5^3 = 1 + 9 + 125 = 135
+#is_disarium(544) ➞ False
+#is_disarium(518) ➞ True
+#is_disarium(466) ➞ False
+#is_disarium(8) ➞ True
 
 def is_disarium(n):
 	str_n = str(n)
